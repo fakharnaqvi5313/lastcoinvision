4973e64523d7b88f5869a03dbd013c438baca3e5
fakharnaqvi5313 <44407654+fakharnaqvi5313@users.noreply.github.com>
2023-03-10 17:01:58 +0330

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Copyright (c) 2013-2019 The Bitcoin Core developers
+#Copyright (c) 2019-2023 The LastcoinVision developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
